1ee2b5493a94d2aaa2bc49d05335dcd9497b5bde
astratow <davno_pora@mail.com>
2022-08-24 06:03:42 +0100

    working on delete functionality

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Dish; //added object
+use App\Form\DishType;
 use App\Repository\DishRepository;
 use Doctrine\Persistence\ManagerRegistry as ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,17 +26,42 @@ class DishController extends AbstractController
     #[Route('/create', name: 'create')]
     public function create(Request $request, ManagerRegistry $doctrine){
         $dish = new Dish();
-        // set name property for Dish object
-        $dish->setName('Pizza');
-        $dish->setDescription('Italian dish, dough base with meat/veges/cheese/seafood on top');
-        // Entity Manager
-        // $entityManager = doctrine->getManager();
-        $entityManager = $doctrine->getManager();
-        // new dish object
-        $entityManager->persist($dish);
-        // send data to database with flush method
-        $entityManager->flush();
+        // form
+        $form = $this->createForm(DishType::class, $dish);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            // $dish = $form->getData();
+            // Entity Manager
+            // $entityManager = doctrine->getManager();
+            $entityManager = $doctrine->getManager();
+            // new dish object
+            $entityManager->persist($dish);
+            // send data to database with flush method
+            $entityManager->flush();
+            return $this->redirect($this->generateUrl('dish.create'));
+        }
+        // $dish->setDescription('Italian dish, dough base with meat/veges/cheese/seafood on top');
+        
         // response
-        return new Response('New Dish has been created.');
+        return $this->render('dish/create.html.twig', [
+            'createForm' => $form->createView()
+        ])
+        // new Response('New Dish has been created.');
+        ;}
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete($id, DishRepository $dishRepository, ManagerRegistry $doctrine)
+    {
+        // Entity Manager
+
+        $entityManager = $doctrine->getManager();
+        $dish = $dishRepository->find($id);
+        $entityManager->remove($dish);
+        $entityManager->flush();
+        $this->addFlash('success', 'Dish was removed successfuly');
+        return $this->redirect($this->generateUrl('dish.create'));
+
     }
+
 }
